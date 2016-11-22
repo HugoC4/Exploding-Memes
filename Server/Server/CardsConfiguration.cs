1c583121840a201a8ec978587162f7d330936cb9
Hugo Kruis <Hugo Kruis>
2016-11-22 14:58:57 +0100

    Server: CardType and CardConfiguration & GetDeck function

--- Server/Server/CardsConfiguration.cs
+++ Server/Server/CardsConfiguration.cs
@@ -1,22 +1,43 @@
 ﻿using Server.Game;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Xml;
 
 namespace Server
 {
     public class CardsConfiguration
     {
+        private static List<Card> Cards { get; set; }
         public static void Initialize()
         {
-            /* Read JSON and create lists */
+            Cards = new List<Card>();
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "cards.xml"));
 
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                Cards.Add(new Card
+                {
+                    Text = node.SelectSingleNode("text").InnerText,
+                    ImageUrl = node.SelectSingleNode("imageUrl").InnerText,
+                    SoundUrl = node.SelectSingleNode("soundUrl").InnerText,
+                    AlwaysUse = node.SelectSingleNode("alwaysUse").InnerText == "true",
+                    Type = (CardType)Enum.Parse(typeof(CardType), node.SelectSingleNode("type").InnerText)
+                });
+            }
         }
 
-        public static List<Card> GetDeck()
+        public static List<Card> GetDeck(int players)
         {
-            return new List<Card>();
+            var deck = Cards;
+            while (deck.Count(p => p.Type == CardType.Explode) > players - 1)
+                deck.Remove(deck.First(p => p.Type == CardType.Explode));
+            while (players < 4 && deck.Count(p => p.Type == CardType.Defuse) > (players + 2))
+                deck.Remove(deck.First(p => p.Type == CardType.Defuse));
+            return deck;
         }
     }
 }
